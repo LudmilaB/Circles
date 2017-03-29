a675f96a805d0a4a81672670fd221792f52a0694
Ludmila <ludonkab@gmail.com>
2017-03-28 18:44:15 -0700

    Trying Cookie Manager

--- js/general.js
+++ js/general.js
@@ -1,6 +1,7 @@
 
 	 
 	function setCookie(cname, cvalue, exdays) {
+	   CookieManager.getInstance().setAcceptCookie(true);
        var d = new Date();
        d.setTime(d.getTime() + (exdays*24*60*60*1000));
        var expires = "expires="+d.toUTCString();
